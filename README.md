3b1401349e8257292700d62b173beac20f71c3e4
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-18 00:46:14 -0700

    gah

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Spdy Keys
+
+The SSL keys stolen from https://github.com/indutny/node-spdy for development usage.
---
+++ index.js
@@ -0,0 +1,8 @@
+
+var fs = require('fs')
+
+module.exports = {
+  key: fs.readFileSync(__dirname + '/keys/spdy-key.pem'),
+  cert: fs.readFileSync(__dirname + '/keys/spdy-cert.pem'),
+  ca: fs.readFileSync(__dirname + '/keys/spdy-csr.pem'),
+}
